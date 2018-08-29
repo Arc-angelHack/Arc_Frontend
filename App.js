13a707a7fdfb2a2ae81cd06349493b7eac825a33
BloomingLight <bloominglightart@gmail.com>
2018-08-28 22:34:50 -0700

    trying to fix the content, bug

--- App.js
+++ App.js
@@ -1,18 +1,61 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Platform } from 'react-native';
+import { Header, Title } from 'native-base'; 
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.viewStyle}>
-        <Text>The Arc App</Text>
-      </View>
-    );
-  }
+
+    //////////////////////////////////////////////
+
+    ////////////////    STATE    ///////////////
+
+    /////////////////////////////////////////////
+
+    state = {
+        currentScreen: ''
+    }
+
+   //////////////////////////////////////////////
+
+    ////////////////  FUCNTIONS   ///////////////
+
+    /////////////////////////////////////////////
+
+    switchScreen = () => {
+
+    }
+
+    //////////////////////////////////////////////
+
+    ////////////////  RENDERING   ///////////////
+
+    /////////////////////////////////////////////
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Header>
+                    <Title>OneArc</Title>
+                </Header>
+                <Text style = {styles.viewStyle}>
+                    This is placeholder text. I NEED TO FIX THIS. Not sure what's wrong.
+                </Text>
+            </View>
+        );
+    }
 }
 
+    //////////////////////////////////////////////
+
+    ////////////////  STYLES   ///////////////
+
+    /////////////////////////////////////////////
+
 const styles = {
-  viewStyle : {
+  container : {
+    flex: 1,
+    marginTop: Platform.OS === 'android' ? 24 : 0
+  },
+  viewStyle: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
