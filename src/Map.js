7f343a3996e5aab9b672f8f9408b09206dc9b392
glowiingo <gloria.nhuto.ngo@gmail.com>
2018-08-30 05:45:01 -0700

    updated Map, Request, and Resource base files

---
+++ src/Map.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import { Button } from 'native-base';
+
+class Map extends React.Component {
+	render () {
+		return (
+
+		)
+	}
+}
+
+export default Map;
---
+++ src/Request.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import { Button } from 'native-base';
+
+class Request extends React.Component {
+	render () {
+		return (
+
+		)
+	}
+}
+
+export default Request;
